0898e7c2b146d647fb5b7ce7ce0f4bef08ebe99c
Parash_Mehara <parasmehra85495@gmail.com>
2025-11-26 09:55:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ async function buyNow(courseName, amount) {
 
         handler: async function (response) {
 
-            const verifyResponse = await fetch("http://localhost:5000/verify-payment", {
+            const verifyResponse = await fetch("https://learntechnotes.onrender.com/verify-payment", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
